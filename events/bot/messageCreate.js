0019fd720801c79ccf84c3501c2203f099d35d7c
LockBlock-dev <LockBlock-dev@protonmail.com>
2021-12-22 13:26:10 +0100

    remove DB fetch at each message

--- events/bot/messageCreate.js
+++ events/bot/messageCreate.js
@@ -5,14 +5,6 @@ module.exports = {
     name: "messageCreate",
     once: false,
     async execute(message, client) {
-        const guild = await client.databaseManagers.guilds.get(message.guild.id);
-        if (!guild) {
-            await client.databaseManagers.guilds.create({
-                id: message.guild.id,
-                name: message.guild.name,
-            });
-        }
-
         await commandHandling(client, message);
         await messageHandling(client, message);
     },
--- utils/messageHandling.js
+++ utils/messageHandling.js
@@ -4,16 +4,28 @@ module.exports = async (client, message) => {
     if (message.author.id === client.user.id || message.channel.type === "DM" || message.system || message.content.indexOf(process.env.PREFIX) === 0)
         return;
 
-    const guild = await client.databaseManagers.guilds.get(message.guild.id);
     const URLregex = /[-a-zA-Z0-9@:%_\+.~#?&//=]+\.[-a-z]+\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/g;
 
     const matches = message.cleanContent.match(URLregex);
     const flagged = [];
     const sussy = [];
 
-    const total = (await client.databaseManagers.stats.get("phishing_attempts")).count;
+    let guild, total;
 
     if (matches) {
+        guild = await client.databaseManagers.guilds.get(message.guild.id);
+
+        if (!guild) {
+            await client.databaseManagers.guilds.create({
+                id: message.guild.id,
+                name: message.guild.name,
+            });
+
+            guild = await client.databaseManagers.guilds.get(message.guild.id);
+        }
+
+        total = (await client.databaseManagers.stats.get("phishing_attempts")).count;
+
         for (let match of matches) {
             let domain = match.split("/").filter((n) => n);
             if (domain[0].includes(":")) domain.shift();
@@ -31,60 +43,62 @@ module.exports = async (client, message) => {
                 }
             }
         }
-    }
 
-    if (flagged.length > 0) {
-        await message.delete();
+        if (flagged.length > 0) {
+            await message.delete();
 
-        await client.databaseManagers.stats.updateCount("phishing_attempts", total + flagged.length);
+            await client.databaseManagers.stats.updateCount("phishing_attempts", total + flagged.length);
 
-        if (guild) {
-            const channel = await message.guild.channels.cache.get(guild.logsChannel);
+            if (guild) {
+                const channel = await message.guild.channels.cache.get(guild.logsChannel);
 
-            if (channel) {
-                const embed = new MessageEmbed()
-                    .setDescription("🚫 Phishing message deleted")
-                    .setThumbnail(message.author.avatarURL({ format: "png" }))
-                    .addField("Channel", `<#${message.channel.id}>`)
-                    .addField("Author", `<@${message.author.id}>`)
-                    .addField("Author id", message.author.id)
-                    .addField(`URL${flagged.length > 1 ? "s" : ""}`, `\`\`\`${flagged.join("\n")}\n\`\`\``)
-                    .addField("Message content", message.cleanContent);
+                if (channel) {
+                    const embed = new MessageEmbed()
+                        .setDescription("🚫 Phishing message deleted")
+                        .setThumbnail(message.author.avatarURL({ format: "png" }))
+                        .addField("Channel", `<#${message.channel.id}>`)
+                        .addField("Author", `<@${message.author.id}>`)
+                        .addField("Author id", message.author.id)
+                        .addField(`URL${flagged.length > 1 ? "s" : ""}`, `\`\`\`${flagged.join("\n")}\n\`\`\``)
+                        .addField("Message content", message.cleanContent);
 
-                await channel.send({ embeds: [embed] });
-            } else {
-                return await message.channel.send({
-                    embeds: [client.newError("Log channel not found! Add one with the command: `p!logs [id]`")],
-                });
+                    await channel.send({ embeds: [embed] });
+                } else {
+                    return await message.channel.send({
+                        embeds: [client.newError("Log channel not found! Add one with the command: `p!logs [id]`")],
+                    });
+                }
             }
-        }
-    } else if (sussy.length > 0) {
-        await message.delete();
+        } else if (sussy.length > 0) {
+            await message.delete();
 
-        await client.databaseManagers.stats.updateCount("phishing_attempts", total + sussy.length);
+            await client.databaseManagers.stats.updateCount("phishing_attempts", total + sussy.length);
 
-        if (guild) {
-            const channel = await message.guild.channels.cache.get(guild.logsChannel);
+            if (guild) {
+                const channel = await message.guild.channels.cache.get(guild.logsChannel);
 
-            if (channel) {
-                const embed = new MessageEmbed()
-                    .setDescription("⚠ Suspicious message deleted")
-                    .setThumbnail(message.author.avatarURL({ format: "png" }))
-                    .addField("Channel", `<#${message.channel.id}>`)
-                    .addField("Author", `<@${message.author.id}>`)
-                    .addField("Author id", message.author.id)
-                    .addField(`Term${sussy.length > 1 ? "s" : ""}`, `\`\`\`${sussy.join("\n")}\n\`\`\``)
-                    .addField("Message content", message.cleanContent);
+                if (channel) {
+                    const embed = new MessageEmbed()
+                        .setDescription("⚠ Suspicious message deleted")
+                        .setThumbnail(message.author.avatarURL({ format: "png" }))
+                        .addField("Channel", `<#${message.channel.id}>`)
+                        .addField("Author", `<@${message.author.id}>`)
+                        .addField("Author id", message.author.id)
+                        .addField(`Term${sussy.length > 1 ? "s" : ""}`, `\`\`\`${sussy.join("\n")}\n\`\`\``)
+                        .addField("Message content", message.cleanContent);
 
-                const warning = await channel.send({ embeds: [embed] });
+                    const warning = await channel.send({ embeds: [embed] });
 
-                await warning.reply(
-                    `Was this warning accurate? Please report the phishing URL${matches.length > 1 ? "s" : ""} by doing \`p!report ${matches[0]}\``
-                );
-            } else {
-                return await message.channel.send({
-                    embeds: [client.newError("Log channel not found! Add one with the command: `p!logs [id]`")],
-                });
+                    await warning.reply(
+                        `Was this warning accurate? Please report the phishing URL${matches.length > 1 ? "s" : ""} by doing \`p!report ${
+                            matches[0]
+                        }\``
+                    );
+                } else {
+                    return await message.channel.send({
+                        embeds: [client.newError("Log channel not found! Add one with the command: `p!logs [id]`")],
+                    });
+                }
             }
         }
     }
