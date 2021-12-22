9229336bbf1a375bd158c9cc5b56fcbbee021139
LockBlock-dev <LockBlock-dev@protonmail.com>
2021-12-22 13:29:37 +0100

    prevent crash

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 
 //client.dev = process.env.PRODUCTION ? false : true;
 
+process.on("uncaughtException", (err) => {
+    console.log(`Uncaught exception: ${err}`);
+});
+
+process.on("unhandledRejection", (err) => {
+    console.log(`Unhandled rejection: ${err}`);
+});
+
 require("./utils/loader.js")(client);
 
 client.login(process.env.DISCORD_TOKEN);
