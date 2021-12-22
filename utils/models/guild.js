4a3245b03df3f49a94beb6b663be6855d022a2eb
LockBlock-dev <LockBlock-dev@protonmail.com>
2021-12-22 21:19:41 +0100

    left field

--- utils/models/guild.js
+++ utils/models/guild.js
@@ -4,6 +4,7 @@ const schema = mongoose.Schema(
     {
         id: { type: String, unique: true },
         name: { type: String },
+        left: { type: Boolean, default: false },
         logsChannel: { type: String, default: null },
     },
     { timestamps: true }
