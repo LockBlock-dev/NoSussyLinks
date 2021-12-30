6f3a55d0a614cbc4ddc0033efad35ffe434d4ac3
LockBlock-dev <LockBlock-dev@protonmail.com>
2021-12-30 18:32:35 +0100

    fix guild delete, activity update

--- events/bot/guildDelete.js
+++ events/bot/guildDelete.js
@@ -2,11 +2,13 @@ module.exports = {
     name: "guildDelete",
     once: false,
     async execute(guild, client) {
-        const res = await client.databaseManagers.guilds.get(message.guild.id);
+        const res = await client.databaseManagers.guilds.get(guild.id);
 
         if (res)
             await client.databaseManagers.guilds.update(guild.id, {
                 left: true,
             });
+
+        client.user.setActivity(`over ${client.guilds.cache.size} servers | p!help`, { type: "WATCHING" });
     },
 };
