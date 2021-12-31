c70d639eddb17d3859bc2a7f721a7a66e4abd80f
LockBlock-dev <LockBlock-dev@protonmail.com>
2021-12-31 15:02:16 +0100

    invited message

--- events/bot/guildCreate.js
+++ events/bot/guildCreate.js
@@ -1,3 +1,5 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
     name: "guildCreate",
     once: false,
@@ -8,5 +10,20 @@ module.exports = {
         });
 
         client.user.setActivity(`over ${client.guilds.cache.size} servers | p!help`, { type: "WATCHING" });
+
+        const welcome = new MessageEmbed()
+            .setDescription("📌 Thanks for using NoSussyLinks!")
+            .addField("To get started please type", "`p!setup`")
+            .setColor("#7289DA");
+
+        if (guild.systemChannel && guild.systemChannel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
+            await guild.systemChannel.send({ embeds: [welcome] });
+        } else {
+            const channel = guild.channels.cache.find(
+                (channel) => channel.type === "GUILD_TEXT" && channel.permissionsFor(guild.me).has("SEND_MESSAGES")
+            );
+
+            await channel.send({ embeds: [welcome] });
+        }
     },
 };
