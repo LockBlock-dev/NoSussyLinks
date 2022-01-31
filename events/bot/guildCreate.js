7c6e092d009e53a0aff3667fc5131450617e515d
LockBlock-dev <LockBlock-dev@protonmail.com>
2022-01-31 19:35:07 +0100

    check to not add guild in DB 2 times

--- events/bot/guildCreate.js
+++ events/bot/guildCreate.js
@@ -4,10 +4,13 @@ module.exports = {
     name: "guildCreate",
     once: false,
     async execute(guild, client) {
-        await client.databaseManagers.guilds.create({
-            id: guild.id,
-            name: guild.name,
-        });
+        const res = await client.databaseManagers.guilds.get(guild.id);
+
+        if (!res)
+            await client.databaseManagers.guilds.create({
+                id: guild.id,
+                name: guild.name,
+            });
 
         client.user.setActivity(`over ${client.guilds.cache.size} servers | p!help`, { type: "WATCHING" });
 
